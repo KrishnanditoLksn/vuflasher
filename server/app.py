2925de8e266d57d4afffb713f85c793529c4715b
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-18 23:34:34 +0700

    feat:moved books list

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
 from flask  import Flask,jsonify
 from flask_cors import CORS
 
+# from utils.util import BOOKS
+
+
+
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -8,11 +12,36 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/books' , methods=['GET'])
+def getAllBooks():
+    return jsonify({
+        'status':'Success',
+        'books' : BOOKS
+    })
 
 if __name__ == '__main__':
     app.run()
